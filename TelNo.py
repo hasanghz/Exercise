0222910e5a405a88e5c02f7aeea525eedc7a0199
hasanghz <ghaznavi.mh@gmail.com>
2025-09-16 12:11:38 +0330

    contact list

--- TelNo.py
+++ TelNo.py
@@ -1,30 +1,16 @@
 from collections import OrderedDict
-def menu():
-    while 1:
 
-
-        print('1.Add a contact.')
-        print('2.Delete a contact.')
-        print('3.Search a contact.')
-        print('4.Change a phone number.')
-        print('5.Change a contact name.')
-        print('6.Show contact list.')
-        print('7.Press 0 to exit.')
-        choose=int(input('Please enter a number : '))
-        while choose<0 or choose>6:
-            choose=int(input('Please enter a number within the range : '))
-        if choose==0:
-            break
-        return choose
     
 def add(telbook):          
     name=input('Please Enter the name of the contact that you want to add : ')
-    number(int('Please enter the number of the contact : '))
+    number=int('Please enter the number of the contact : ')
     telbook[name]=number
+    print(telbook)
 
 def delete(telbook):
     name=input('Please enter the name of the contact that you want to delete : ')
     del telbook[name]
+    print(telbook)
 
 def search(telbook):
     name=input('Please enter the contact you want to find : ')
@@ -36,11 +22,13 @@ def change_number(telbook):
     contact={}
     contact[name]=new_num
     telbook.update(contact)
+    print(telbook)
 
 def change_name(telbook):
     number=int(input('Please enter the number that you want to change its name : '))
     new_name=input('Please enter the new name : ')
     telbook[new_name]=number
+    print(telbook)
 
     if number in telbook.values():
         del telbook[telbook.keys(number)]
@@ -51,16 +39,45 @@ def show_all_contacts(telbook):
 
 
 
-
-
-
-
-
-
-
 n=int(input('Please enter the number of the contact : '))
-telbook={}
+telbook=OrderedDict()
 for i in range (n):
     name=input('Please enter the contact name : ')
     number=input('Please enter the contact number : ')
     telbook[name]=number
+
+
+while 1:
+
+
+    print('1.Add a contact.')
+    print('2.Delete a contact.')
+    print('3.Search a contact.')
+    print('4.Change a phone number.')
+    print('5.Change a contact name.')
+    print('6.Show contact list.')
+    print('7.Press 0 to exit.')
+    choose=int(input('Please enter a number : '))
+    while choose<0 or choose>6:
+        choose=int(input('Please enter a number within the range : '))
+    if choose==0:
+        break
+    if choose==1:
+        add(telbook)
+
+    if choose==2:
+        delete(telbook)
+    
+    if choose==3:
+        search(telbook)
+    
+    if choose==4:
+        change_number(telbook)
+
+    if choose==5:
+        change_name(telbook)
+
+    if choose==6:
+        show_all_contacts(telbook)
+
+    
