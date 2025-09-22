978d925e6fba47ff973724514bbdf7df51c121d5
hasanghz <ghaznavi.mh@gmail.com>
2025-09-22 12:01:01 +0330

    Calculating employee's Tax and Insurance fee

--- Class_2.py
+++ Class_2.py
@@ -7,26 +7,24 @@ class employee:
             
     def caltax(self):
         if self.salary<=1500:
-            print('Tax is 0')
+            return 0
         else:
-            print('Tax is : ',(self.salary-1500)*0.1)
-        return ((self.salary-1500)*0.1)
+            
+            return ((self.salary-1500)*0.1)
     
     def calinsurance(self):
-        print('The insurance fee is : ',self.salary/10)
+        
         return(self.salary*0.1)
     
     def pay(self):
-        return (self.salary-self.salary.calinsurance-self.salary.caltax)
+        return (self.salary-self.calinsurance()-self.caltax())
         
 
     def __str__(self):
-        return f'The salary is : {self.salary.pay()}'
+        return f'The employee\'s name is {self.name,self.family} \n The salary is : {self.pay()} \n The Insurance fee is {self.calinsurance()} \n The tax fee is : {self.caltax()}'
 
 
 p1=employee(1,'Hasan','Ghaznavi',2000)
 p2=employee(2,'Shiva','Alipour',1000)
-print(p1.calinsurance())
-print(p2.calinsurance())
 
-print(p1)
+print(str(p1),'\n',str(p2))
