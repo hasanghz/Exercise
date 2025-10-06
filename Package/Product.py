b6b6a8ce7fec19624145e63da4b2698fbb8264d1
hasanghz <ghaznavi.mh@gmail.com>
2025-10-06 11:32:52 +0330

    Product Class

--- Package/Product.py
+++ Package/Product.py
@@ -1,3 +1,30 @@
-import os
-os.mkdir('Package')
-os.chdir('package')
+class Product:
+    def __init__(self,name,price,discount):
+        self.name=name
+        self.price=price
+        self.discount=discount
+    
+    def __str__(self):
+        s=f'Product name is {self.name} \n Price is {self.price} \n {self.discount}'
+        return s
+    
+    def __repr__(self):
+        return self.__str__()
+pr=[]
+tr=[] 
+kount=int(input('Please enter the number of products :'))
+for i in range (kount):
+    p=input('Please enter the product information in order of name, price and discount :')
+    lp=p.split()
+    pr.append(Product(lp[0],lp[1],lp[2]))
+    tr.append(lp)
+
+for x in range(len(pr)):
+    print(pr[x].name)
+    print(pr)
+    print(tr)
+    print(tr[x][0])
+
+
+
+
