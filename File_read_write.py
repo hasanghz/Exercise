3fb64a51f3952ca4b52361d0cd6f9955e3649b30
hasanghz <ghaznavi.mh@gmail.com>
2025-10-09 11:41:25 +0330

    4rd Edition

--- File_read_write.py
+++ File_read_write.py
@@ -1,13 +1,14 @@
-file1=open('D:\\Python\\Exercise\\Exercise\\file1.txt','wt')
+file1=open('D:\\Python\\Exercise\\Exercise\\file1.txt','r+')
 s1='Im the file 1 and im ready to exchange my data with file 2'
 file1.write(s1)
-file2=open('D:\\Python\\Exercise\\Exercise\\file2.txt','wt')
+file2=open('D:\\Python\\Exercise\\Exercise\\file2.txt','w+')
 s2='Hello im file2 and im ready to tranfer my data to file 1'
 file2.write(s2)
 file2.close()
 
-file2=open('D:\\Python\\Exercise\\Exercise\\file2.tex','tw')
+file2=open('D:\\Python\\Exercise\\Exercise\\file2.txt','tw')
 while 1:
+    
     s3=file1.readline()
     if len(s1)==0:
         break
