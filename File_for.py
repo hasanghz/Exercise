06ef75f63fd15606d70024355750c7d56e9f8b47
hasanghz <ghaznavi.mh@gmail.com>
2025-10-14 12:37:20 +0330

    first edition

---
+++ File_for.py
@@ -0,0 +1,18 @@
+def for_file(ls,number):
+    main_file=open('D:\\Python\\Exercise\\Exercise\\text5.txt','wt')
+    for i in range(number):
+        t=open(ls[i])
+        for line in open(t):
+            main_file.write(line)
+    main_file.close()
+
+lis=[]
+number=int(input('Please enter the number of files : '))
+for i in range(number):
+    x=input('Please enter the address of file : ')
+    lis.append(x)
+
+for_file(lis,number)
+
+
+
