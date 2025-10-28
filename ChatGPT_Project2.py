e2994ec41713eed8e991fac257ba0e529ba9220d
hasanghz <ghaznavi.mh@gmail.com>
2025-10-28 12:04:52 +0330

    Calculating expire date of Medicines

--- ChatGPT_Project2.py
+++ ChatGPT_Project2.py
@@ -6,6 +6,17 @@ time_split=list(map(int, time_split))
 
 for line in myfile.readlines():
     lis=line.split(',')
-    lis[1]=lis[1].split
-    print(lis[1])
-    print(time_split)
+   # lis[1]=lis[1].split()
+    expire=lis[1].split('-')
+    expire=list(map(int, expire))
+    expire_day=(expire[0]*365)+(expire[1]*30)+(expire[2])
+    time_day=(time_split[0]*365)+(time_split[1]*30)+(time_split[2])
+    if expire_day-time_day <0 :
+        print(f'{lis[0]} has expired')
+    elif expire_day-time_day >0 and expire_day-time_day<90 :
+        print(f'{lis[0]} will expire soon !')
+    elif expire_day-time_day > 90:
+        print(f'{lis[0]} expire date is valid')
+    
+   
+
