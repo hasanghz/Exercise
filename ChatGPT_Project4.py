0ee1f4ed67a5f6f4c3e1e17cd6eae3a065fe7f72
hasanghz <ghaznavi.mh@gmail.com>
2025-11-16 11:54:12 +0330

    Final edition

--- ChatGPT_Project4.py
+++ ChatGPT_Project4.py
@@ -11,19 +11,21 @@ with open('d:\\python\\exercise\\exercise\\sales_data.txt') as myfile:
         
 with open('d:\\python\\exercise\exercise\Results_project4.txt','wt') as result:
     sum=0
+    x=0
     mean=0
     lowest=0
+    lowest_2=0
     highest=0
     lis_values=dic.values()
     for n in lis_values:
+        
         sum+=n
+        
         mean=sum/len(dic)
-        if lowest>n:
-            lowest=n
-        if n> highest:
-            highest=n
+        lowest=min(dic.values())
+        highest=max(dic.values())
 
     
     
-    result.write(f'The number of valid values is : {len(dic)} \nThe sum is : {sum} \nThe mean is : {mean}\nThe lowest is : {lowest}\n The highest is : {highest} ')
+    result.write(f'The number of valid values is : {len(dic)} \nThe sum is : {sum} \nThe mean is : {mean}\nThe lowest is : {lowest}\nThe highest is : {highest} ')
     print(dic, '    ',lis_values)
